aaabc846a52dd0f8f8b5e0546e68d55f640eb86f
christinapanto <cp15287@my.bristol.ac.uk>
2015-11-06 21:26:53 +0000

    Update display.c

--- display.c
+++ display.c
@@ -1,26 +1,63 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <SDL2/SDL.h>
-int main(int argc,char** argv)
-{
-    /*Marshall is cool*/
-    argc = argc;
-    argv = argv;
-    SDL_Init(SDL_INIT_VIDEO);//初始化SDL
-    SDL_Surface* img=SDL_LoadBMP("Restuarant.bmp");//加载图片到内存
-    SDL_Window* win=SDL_CreateWindow("niconiconi", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 600, 400,SDL_WINDOW_SHOWN);//创建一个标题为niconiconi，大小800x600的窗口，最后一个参数表示该窗口创建完毕后立即显示
-    SDL_Renderer* ren =SDL_CreateRenderer(win, -1,SDL_RENDERER_ACCELERATED);//为窗口创建渲染器，第三个参数表示该渲染器使用硬件来加速渲染
+
+    /*Store everything needed for displaying graphics.  The images for entities
+    are stored in an array indexed by character code*/
+    struct display {
+        int width, height, imageWidth, imageHeight;
+        SDL_Window *window;
+        SDL_Surface *surface;
+        SDL_Surface *images[128];
+    };
     
-    SDL_Texture* img_tex=SDL_CreateTextureFromSurface(ren,img);//以内存中的图片为来源创建一个一模一样的贴图
-    SDL_FreeSurface(img);//img没用了，释放它节省内存;
-    /*Mona pretty*/
-    SDL_RenderClear(ren);//清理一下渲染器
-    SDL_RenderCopy(ren,img_tex,NULL,NULL);//将贴图复制到渲染器
-    SDL_RenderPresent(ren);//显示渲染器
-    SDL_Delay(15000);//延迟3秒
+    display *newDisplay(int width, int height) {
+    display *d = malloc(sizeof(struct display));
+    d->width = width=30;
+    d->height = height=12;
+    d->imageHeight = d->imageWidth = 20;
+    int result = SDL_Init(SDL_INIT_VIDEO);
+    if (result < 0){
+        SDL_Fail("Bad SDL");
+    }
+    int w = d->width * d->imageWidth, h = d->height * d->imageHeight;
+    d->window = SDL_CreateWindow("Restaurant", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 600, 400,SDL_WINDOW_SHOWN);
+    if (d->window == NULL){
+        SDL_Fail("Bad window");
+    }
+    d->surface = SDL_GetWindowSurface(d->window);
+    if (d->surface == NULL) {
+        SDL_Fail("Bad surface");
+    }
+    /*loadAllImages(d);*/
+    return d;
+}
+ 
+ 
+    /* Load all the images we will be using durong the game.
+    static void loadAllImages(display *d) {
+    loadImage(d, '.', "alien1.bmp");
+    loadImage(d, '#', "alien2.bmp");
+    loadImage(d, '*', "alien3.bmp");
+    loadImage(d, '@', "player.bmp");
+}   */
     
-    SDL_DestroyTexture(img_tex);//不用的贴图要及时销毁
-    SDL_DestroyRenderer(ren);//要退出了，销毁渲染器
-    SDL_DestroyWindow(win);//同上，销毁窗口
-    SDL_Quit();//退出
+    /*Uplodaing the image*/
+    SDL_Surface* img=SDL_LoadBMP("Restuarant.bmp");
+    /*Testing if we read the image or not*/
+    if (img == NULL) SDL_Fail("Bad image file");
+    
+    SDL_Renderer* ren =SDL_CreateRenderer(win, -1,SDL_RENDERER_ACCELERATED);
+    SDL_Texture* img_tex=SDL_CreateTextureFromSurface(ren,img);
+    SDL_FreeSurface(img);
+    SDL_RenderClear(ren);
+    //SDL_RenderCopy(ren,img_tex,NULL,NULL);//
+    SDL_RenderPresent(ren);//
+    SDL_Delay(15000);//
+    
+    SDL_DestroyTexture(img_tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
     return 0;
 }
